8ff1b01963aaf0f59bcf463a7f4a8f084ff24123
manuelfernando <manuelcamaron@hotmail.com>
2018-01-31 19:36:52 -0500

    split step5

---
+++ step5/chatApp2.js
@@ -0,0 +1,19 @@
+angular.module('chatApp', ['open-chat-framework'])
+  .run(['$rootScope', 'ngChatEngine', function($rootScope, ngChatEngine) {
+
+    ...
+
+  }])
+  .controller('chatAppController', function($scope) {
+    $scope.ChatEngine.connect(new Date().getTime(), {}, 'auth-key');
+
+    $scope.ChatEngine.on('$.ready', (data) => {
+      $scope.me = data.me;
+
+      $scope.me.plugin(ChatEngineCore.plugin['chat-engine-random-username']($scope.ChatEngine.global));
+
+      // bind chat to updates
+      $scope.chat = $scope.ChatEngine.global;
+      $scope.chat.plugin(ChatEngineCore.plugin['chat-engine-online-user-search']({ prop: 'state.username' }));
+    });
+  });
